fc6760f854c03048fe1a7e241d84f797f8e8d276
Caden <Caden@slitandrewind.com>
2024-11-11 16:22:26 -0700

    Removed extra Platforms

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HSPGUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
